2e9ceb7c0479a909bde31d1b4a876a9f51827910
ikjyot <isg6@njit.edu>
2017-09-05 19:25:04 -0400

    recipe item has been seperated from recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,8 @@ import {Recipe} from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Test recipe', 'This is a tasty test recipe',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-04.jpg'),
+      new Recipe('Test recipe', 'This is a tasty test recipe',
       'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-04.jpg')
   ];
 
